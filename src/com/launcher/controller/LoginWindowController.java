8c091e712b1d50bfe883f93b756f26fe57f76509
Maciej Janczewski <maciej.janczewski.programista@gmail.com>
2022-04-22 10:04:54 +0200

    Add JavaMail

--- src/com/launcher/controller/LoginWindowController.java
+++ src/com/launcher/controller/LoginWindowController.java
@@ -1,6 +1,8 @@
 package com.launcher.controller;
 
 import com.launcher.EmailManager;
+import com.launcher.controller.services.LoginService;
+import com.launcher.model.EmailAccount;
 import com.launcher.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -30,10 +32,38 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction(){
+        if(fieldsAreValid()){
+            EmailAccount emailAccount= new EmailAccount(emailAddressField.getText(), passwordField.getText());
+            LoginService loginService= new LoginService(emailAccount,emailManager);
+            EmailLoginResult emailLoginResult= loginService.login();
+
+            switch(emailLoginResult){
+                case SUCCESS:
+                    System.out.println("login succefull!!!!"+ emailAccount);
+                return;
+
+            }
+        }
         System.out.println("loginButtonAction");
         viewFactory.showMainWindow();
         Stage stage= (Stage) errorLabel.getScene().getWindow();
         viewFactory.closeStage(stage);
+
+    }
+
+    private boolean fieldsAreValid() {
+        if(emailAddressField.getText().isEmpty()){
+            errorLabel.setText("Please fill email");
+            return false;
+
+        }
+
+        if(passwordField.getText().isEmpty()){
+            errorLabel.setText("Please fill password");
+            return false;
+
+        }
+        return true;
 
     }
 
